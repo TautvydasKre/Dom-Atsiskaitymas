b4c5ce00c250d289c90ee355537df1ae092770f9
Arnas Simaitis <113042490+ArnasSimairis@users.noreply.github.com>
2023-12-20 13:18:04 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,56 @@
+const doc = document;
+
+//  Uzduotis Nr.1
+
+// const div = doc.createElement("div");
+// doc.body.appendChild(div);
+// const btn1 = doc.createElement("button");
+// div.appendChild(btn1);
+// const btn2 = doc.createElement("button");
+// div.appendChild(btn2);
+// const para = doc.createElement("p");
+// btn1.textContent = "btnUp";
+// btn2.textContent = "btnDown";
+// btn1.setAttribute("class", "btnUp");
+// btn2.setAttribute("class", "btnDown");
+
+// div.appendChild(para);
+
+// const buttonStyle =
+//   "width: 150px; height: 20px; margin-right: 30px; border-radius: 25px; background-color: yellow; color: black";
+// btn1.style = buttonStyle;
+// btn2.style = buttonStyle;
+
+// let countClicks = 0;
+// btn1.addEventListener("click", (event) => {
+//   event.preventDefault();
+//   countClicks++;
+//   console.log(countClicks);
+
+//   para.textContent = countClicks;
+//   if (para.textContent % 2 === 0) {
+//     btn1.style.backgroundColor = "red";
+//   } else {
+//     btn1.style.backgroundColor = "yellow";
+//   }
+// });
+
+// btn2.addEventListener("click", (event) => {
+//   event.preventDefault();
+//   countClicks--;
+//   console.log(countClicks);
+
+//   para.textContent = countClicks;
+//   if (para.textContent % 2 === 0) {
+//     btn2.style.backgroundColor = "red";
+//   } else {
+//     btn2.style.backgroundColor = "yellow";
+//   }
+// });
+
+//  Uzduotis Nr.2
+
+
+// const div = doc.createElement("div")
+// const firstInt = doc.createElement("input")
+// const secondInt = doc.createElement("input")
